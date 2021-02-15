da8fe30433054d415459ba37bf18f5197d8c4c82
sautmanurung <smanurung360@gmail.com>
2021-02-15 23:51:15 +0700

    baru 2

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,34 +2,37 @@
 	
 @section('isi')
 <div class="col-md-8 hot-post-left">
-					<!-- post -->
+
+					
 					<div class="post post-thumb">
-						<a class="post-img" href="blog-post.html"><img src="{{ asset('public/frontend/img/hot-post-1.jpg')}}" alt=""></a>
+					<!-- post -->
+						<a class="post-img lazy-load" href=""><img src="" alt="" style=""></a>
 						<div class="post-body">
 							<div class="post-category">
-								<a href="category.html">Lifestyle</a>
+								<a href=""></a>
 							</div>
-							<h3 class="post-title title-lg"><a href="blog-post.html">Postea senserit id eos, vivendo periculis ei qui</a></h3>
+							<h3 class="post-title title-lg"><a href=""></a></h3>
 							<ul class="post-meta">
-								<li><a href="author.html">John Doe</a></li>
-								<li>20 April 2018</li>
+								<li><a href=""></a></li>
+								<li></li>
 							</ul>
 						</div>
+					
 					</div>
 					<!-- /post -->
 				</div>
 				<div class="col-md-4 hot-post-right">
 					<!-- post -->
 					<div class="post post-thumb">
-						<a class="post-img" href="blog-post.html"><img src="{{ asset('public/frontend/img/hot-post-2.jpg')}}" alt=""></a>
+						<a class="post-img lazy-load" href=""><img src="" alt=""></a>
 						<div class="post-body">
 							<div class="post-category">
-								<a href="category.html">Lifestyle</a>
+								<a href=""></a>
 							</div>
-							<h3 class="post-title"><a href="blog-post.html">Sed ut perspiciatis, unde omnis iste natus error sit</a></h3>
+							<h3 class="post-title"><a href=""></a></h3>
 							<ul class="post-meta">
-								<li><a href="author.html">John Doe</a></li>
-								<li>20 April 2018</li>
+								<li><a href=""></a></li>
+								<li></li>
 							</ul>
 						</div>
 					</div>
@@ -37,22 +40,21 @@
 
 					<!-- post -->
 					<div class="post post-thumb">
-						<a class="post-img" href="blog-post.html"><img src="{{ asset('public/frontend/img/hot-post-3.jpg')}}" alt=""></a>
+						<a class="post-img" href=""><img src="" alt=""></a>
 						<div class="post-body">
 							<div class="post-category">
-								<a href="category.html">Fashion</a>
-								<a href="category.html">Lifestyle</a>
+								<a href=""></a>
+								<a href=""></a>
 							</div>
-							<h3 class="post-title"><a href="blog-post.html">Mel ut impetus suscipit tincidunt. Cum id ullum laboramus persequeris.</a></h3>
+							<h3 class="post-title"><a href=""></a></h3>
 							<ul class="post-meta">
-								<li><a href="author.html">John Doe</a></li>
-								<li>20 April 2018</li>
+								<li><a href=""></a></li>
+								<li></li>
 							</ul>
 						</div>
 					</div>
 					<!-- /post -->
 				</div>
-	
 		<!-- /container -->
 	</div>
 	<!-- /SECTION -->
@@ -78,9 +80,9 @@
 								<a class="post-img" href="{{ route('blog.isi', $post_terbaru->slug ) }}"><img src="{{ $post_terbaru->gambar }}" alt="" style="height: 200px"></a>
 								<div class="post-body">
 									<div class="post-category">
-										<a href="#">{{ $post_terbaru->category->name }}</a>
+										<a href="{{ route('blog.category', $post_terbaru->category->name ) }}">{{ $post_terbaru->category->name }}</a>
 									</div>
-									<h3 class="post-title"><a href="#">{{ $post_terbaru->judul }}</a></h3>
+									<h3 class="post-title"><a href="{{ route('blog.isi', $post_terbaru->slug ) }}">{{ $post_terbaru->judul }}</a></h3>
 									<ul class="post-meta">
 										<li><a href="#">{{ $post_terbaru->users->name }}</a></li>
 										<li>{{ $post_terbaru->created_at->diffForHumans() }}</li>
@@ -100,6 +102,7 @@
 		
 			<!-- /row -->
 @endsection
+
 
 
 
